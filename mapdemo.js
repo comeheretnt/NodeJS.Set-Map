08c89e8dda0a116d712773eb2619bc0b8a0122dd
comeheretnt <minh.pham220208@vnuk.edu.vn>
2024-10-22 18:45:51 +0700

    first commit

---
+++ mapdemo.js
@@ -0,0 +1,17 @@
+
+const students = new Map();
+
+students.set(1, { name: 'Alice', age: 20 });
+students.set(2, { name: 'Bob', age: 22 });
+students.set(3, { name: 'Charlie', age: 21 });
+
+console.log(students.get(1)); 
+
+console.log(students.has(2)); 
+
+students.delete(3);
+console.log(students); 
+
+students.forEach((info, id) => {
+  console.log(`ID: ${id}, Tên: ${info.name}, Tuổi: ${info.age}`);
+});
---
+++ setdemo.js
@@ -0,0 +1,19 @@
+
+const friends = new Set();
+
+friends.add('Alice');
+friends.add('Bob');
+friends.add('Charlie');
+friends.add('Alice'); 
+
+console.log(friends); 
+
+console.log(friends.has('Bob')); 
+
+
+friends.delete('Charlie');
+console.log(friends); 
+
+friends.forEach(friend => {
+  console.log(`Bạn của tôi: ${friend}`);
+});
